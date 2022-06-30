bcc6a961acec7802e4ce8541cd7c6b0e4ab4f0a0
Andrea Gherbi <gherbiandrea@gmail.com>
2022-06-30 18:42:14 -0300

    modifico main para probar parte 1 y modificar informe

--- src/tpe/TPE.java
+++ src/tpe/TPE.java
@@ -16,21 +16,23 @@ public class TPE {
 
 
     	//Parte 2
-    	   Timer temporizador = new Timer();
-    	   GrafoGeneros grafo = new GrafoGeneros();
-    	   
-    	   //final String path = "C:\\Users\\blase\\Desktop\\Prog3\\src\\tpe/assets/csv2/dataset2.csv";
-    	   final String path = "C://Users/Andrea/eclipse-workspace/TPE/TPEProg3/src/tpe/assets/csv2/dataset3.csv";
-    	   temporizador.start();
-    	   
-    	   CSVReader.readGrafo(grafo, path);
-           System.out.println(temporizador.stop() + " milisegundos de carga de datos") ;
-
-           System.out.println();
-          // System.out.println(grafo);
-
-         //Servicio 1: Obtener los N géneros más buscados luego de buscar por el género A.
-           System.out.println(grafo.generosMasBuscados("negocios", 5)) ;
+		/*
+		 * Timer temporizador = new Timer(); GrafoGeneros grafo = new GrafoGeneros();
+		 * 
+		 * //final String path =
+		 * "C:\\Users\\blase\\Desktop\\Prog3\\src\\tpe/assets/csv2/dataset2.csv"; final
+		 * String path =
+		 * "C://Users/Andrea/eclipse-workspace/TPE/TPEProg3/src/tpe/assets/csv2/dataset3.csv";
+		 * temporizador.start();
+		 * 
+		 * CSVReader.readGrafo(grafo, path); System.out.println(temporizador.stop() +
+		 * " milisegundos de carga de datos") ;
+		 * 
+		 * System.out.println(); // System.out.println(grafo);
+		 * 
+		 * //Servicio 1: Obtener los N géneros más buscados luego de buscar por el
+		 * género A. System.out.println(grafo.generosMasBuscados("negocios", 5)) ;
+		 */
            
          //Servicio 2: A partir de un género A encontrar, en tiempo polinomial, la secuencia 
          //de géneros que más alto valor de búsqueda posee
@@ -48,13 +50,13 @@ public class TPE {
 			 */
 
     	
-        /* Parte 1
+        //Parte 1
         Timer temporizador = new Timer();
         List<Libro> libros = new LinkedList<>();
         Indice indice = new Indice();
 
       //public static final String PATH = "c:/Users/blase/Desktop/Prog3TPE/tpe/assets/csv/dataset4.csv";
-    	final String path = "C://Users/Andrea/eclipse-workspace/TPE/TPEProg3/src/tpe/assets/csv/dataset2.csv";
+    	final String path = "C://Users/Andrea/eclipse-workspace/TPE/TPEProg3/src/tpe/assets/csv/dataset1.csv";
         final String pathSalida = "C://Users/Andrea/eclipse-workspace/TPE/TPEProg3/src/tpe/assets/csv/salida.csv";     	
         temporizador.start();
         CSVReader.read(libros,indice, path);
@@ -75,12 +77,8 @@ public class TPE {
         } catch (IOException e) {
             e.printStackTrace();
         }
-*/
-
-        
+     
 
     }
-
-
 
 }
